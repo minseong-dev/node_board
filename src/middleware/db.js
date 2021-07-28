5168bcf1c7fd29aa72af5acb2fb819d0f370a445
minseong-dev <aa971020@gmail.com>
2021-07-28 17:03:04 +0900

    DB연결

--- src/middleware/db.js
+++ src/middleware/db.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host : 'localhost',
     user : 'root',
     password : '1234',
-    database : 'board',
+    database : 'boardDB',
 })
 
 async function query(sql, args) {
